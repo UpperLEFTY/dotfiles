330e180e51f6835a6276b5376f5c052b39be1d98
Essence Alexander <upperlefty@zoho.eu>
2023-09-02 01:36:18 -0700

    miscellaneous for linux

---
+++ src/os/installs/ubuntu/misc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# shellcheck disable=SC1091
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../../utils.sh" \
+    && . "./utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n   Miscellaneous\n\n"
+
+install_package "VLC" "vlc"
