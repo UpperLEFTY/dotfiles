d2f74b676dd9f279a3a97927b978ac8a31738337
Essence Alexander <upperlefty@zoho.eu>
2023-09-02 01:31:14 -0700

    quick cleanup

---
+++ src/os/installs/ubuntu/cleanup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# shellcheck disable=SC1091
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "./utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n   Cleanup\n\n"
+autoremove
