897df5a000f93e0175486e9f5c512b91fbf24737
Essence Alexander <upperlefty@zoho.eu>
2023-09-02 01:21:37 -0700

    vscode install

---
+++ src/os/installs/macos/vscode.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# shellcheck disable=SC1091
+
+cd "$(dirname "${ZSH_SOURCE[0]}")" \
+    && . "../../utils.sh" \
+    && . "./utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+install_plugin() {
+    execute "code --install-extension $2" "$1 plugin"
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n   Visual Studio Code\n\n"                                :
+
+# Install VSCode
+brew_install "Visual Studio Code" "visual-studio-code" "--cask"
+
+printf "\n"
+
+# Install the VSCode plugins
+install_plugin "EditorConfig" "EditorConfig.EditorConfig"
+install_plugin "File Icons" "vscode-icons-team.vscode-icons"
+install_plugin "MarkdownLint" "DavidAnson.vscode-markdownlint"
+
+# Close VSCode
+osascript -e 'quit app "Visual Studio Code"'
