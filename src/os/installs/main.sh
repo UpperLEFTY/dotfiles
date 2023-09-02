85c0b73a10930e9363f0e23fce6aa7ae6ee6c401
Essence Alexander <upperlefty@zoho.eu>
2023-09-02 01:46:13 -0700

    installs

---
+++ src/os/installs/main.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# shellcheck disable=SC1091
+
+cd "$(dirname "${ZSH_SOURCE[0]}")" \
+    && . "../utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n • Installs\n\n"
+
+"./$(get_os)/main.sh"
