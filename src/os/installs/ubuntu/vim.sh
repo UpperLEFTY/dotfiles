6dfe91076b6013124acfdcafc45e8c4a6958da4c
Essence Alexander <upperlefty@zoho.eu>
2023-09-02 01:41:48 -0700

    vim install

---
+++ src/os/installs/ubuntu/vim.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# shellcheck disable=SC1091
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "./utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+install_package "Vim" "vim-gtk3"
