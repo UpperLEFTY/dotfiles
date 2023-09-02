e925843cdd42204b305f6879bfd8ded2235577f7
Essence Alexander <upperlefty@zoho.eu>
2023-09-02 00:47:53 -0700

    altern tools script

---
+++ src/os/installs/macos/misc_tools.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# shellcheck disable=SC1091
+cd "$(dirname "${ZSH_SOURCE[0]}")" \
+    && . "../../utils.sh" \
+    && . "./utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n   Miscellaneous Tools\n\n"
+
+brew_install "ShellCheck" "shellcheck"
+
+if [ -d "$HOME/.nvm" ]; then
+    brew_install "Yarn" "yarn"
+fi
