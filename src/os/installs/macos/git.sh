c4c5f101a941a5d80e2623bf08ff9a9e8a1c2f7b
Essence Alexander <upperlefty@zoho.eu>
2023-09-02 00:15:04 -0700

    git install script

---
+++ src/os/installs/macos/git.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# shellcheck disable=SC1091
+cd "$(dirname "${ZSH_SOURCE[0]}")" \
+    && . "../../utils.sh" \
+    && . "./utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n   Git\n\n"
+
+brew_install "Git" "git"
+brew_install "GitHub CLI" "gh"
