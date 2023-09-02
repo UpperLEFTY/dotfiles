5755f9d421649a55f54a7d84cea6c9124f6c6831
Essence Alexander <upperlefty@zoho.eu>
2023-09-02 01:47:58 -0700

    npm script

---
+++ src/os/installs/npm.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# shellcheck disable=SC1091
+
+cd "$(dirname "${ZSH_SOURCE[0]}")" \
+    && . "../utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+install_npm_package() {
+
+    execute \
+        ". $HOME/.zsh.local \
+            && npm install --global --silent $2" \
+        "$1"
+
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    print_in_purple "\n   npm\n\n"
+
+    install_npm_package "Update npm" "npm"
+
+}
+
+main
