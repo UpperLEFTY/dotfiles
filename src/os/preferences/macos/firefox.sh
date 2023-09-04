3b8b212443dedb2c9130e03b6679d3e9755ef2b8
Essence Alexander <upperlefty@zoho.eu>
2023-09-03 19:38:54 -0700

    firefox script

---
+++ src/os/preferences/macos/firefox.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# shellcheck disable=SC1091
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../../utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n   Firefox\n\n"
+
+execute "defaults write org.mozilla.firefox AppleEnableSwipeNavigateWithScrolls -bool false" \
+    "Disable backswipe"
+
+killall "firefox" &> /dev/null
