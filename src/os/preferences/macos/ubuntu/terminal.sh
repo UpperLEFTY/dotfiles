742a6d5ac7bbebcbfc61368312da24870b710512
Essence Alexander <upperlefty@zoho.eu>
2023-09-04 22:47:52 -0700

    set theme

---
+++ src/os/preferences/macos/ubuntu/terminal.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# shellcheck disable=SC1091
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../../utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n   Terminal\n\n"
+
+execute "gsettings set org.gnome.desktop.interface monospace-font-name 'Monospace 12'" \
+    "Change font size"
+
+execute "./set_terminal_theme.sh" \
+    "Set custom terminal theme"
