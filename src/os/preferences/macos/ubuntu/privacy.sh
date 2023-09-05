973aeb2ad7ff24c46cf1c1682f4f432afb491620
Essence Alexander <upperlefty@zoho.eu>
2023-09-04 22:42:43 -0700

    turn off remote search

---
+++ src/os/preferences/macos/ubuntu/privacy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# shellcheck disable=SC1091
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../../utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n   Privacy\n\n"
+
+execute "gsettings set com.canonical.Unity.Lenses remote-content-search 'none'" \
+    "Turn off 'Remote Search' so that search terms in Dash do not get sent over the internet"
