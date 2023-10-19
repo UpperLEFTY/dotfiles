544ecff645b0d15dca9dcbc06cc1068fc19b3f99
Essence Alexander <upperlefty@zoho.eu>
2023-10-18 17:20:03 -0700

    added install for tmux

---
+++ src/os/installs/macos/tmux.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# shellcheck disable=SC1091
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../../utils.sh" \
+    && . "./utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n   tmux\n\n"
+
+brew_install "tmux" "tmux"
+brew_install "Pasteboard" "reattach-to-user-namespace"
